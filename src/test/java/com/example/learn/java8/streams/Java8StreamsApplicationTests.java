b2770b45f4fe533c235759fc151e7ecbcc0a0097
Robin Rawat <robin.rawat@hashstudioz.com>
2022-06-08 11:23:40 +0530

    Initial commit

---
+++ src/test/java/com/example/learn/java8/streams/Java8StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn.java8.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
