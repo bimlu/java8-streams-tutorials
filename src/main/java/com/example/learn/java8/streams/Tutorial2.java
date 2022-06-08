cf643b81ed49d7cece6832d5dedea04b02cb295e
Robin Rawat <robin.rawat@hashstudioz.com>
2022-06-08 12:05:10 +0530

    Restructure project

---
+++ src/main/java/com/example/learn/java8/streams/Tutorial2.java
@@ -0,0 +1,17 @@
+package com.example.learn.java8.streams;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tutorial2 {
+
+    public void run() {
+
+        System.out.println("\n***********************************");
+        System.out.println("https://baeldung.com/java-8-streams");
+        System.out.println("***********************************\n");
+
+
+
+    }
+}
